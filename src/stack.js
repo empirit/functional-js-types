babb458ab3efd68ef1146deb91b85ba3212f415d
Oleg <oleg@empirit.com>
2020-05-30 16:55:25 -0400

    fixed Stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const {head, drop, prepend} = require ('ramda')
+const {over, lensIndex, head, drop, prepend} = require ('ramda')
 const State = require ('./state.js')
 
 const Stack = State
@@ -8,7 +8,8 @@ Stack.get = Stack (s => [s, s])
 State.modify = f => Stack(s => [null, f(s)])
 State.put = x => Stack(s => [null, x])
 
-Stack.peak = Stack (s => [head (s), s])
+Stack.peek = Stack (s => [head (s), s])
+Stack.update = f => Stack (s => [null, over (lensIndex(0), f, s)])
 Stack.fetch = f => Stack (s => [f (s), s])
 Stack.pop = Stack (s => [head (s), drop (1, s)])
 Stack.push = x => Stack (s => [null, prepend (x, s)])
